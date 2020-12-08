43d77f890cbb848f10735e655d3b0d8be1afa069
1zielinski1 <zielinski.of@gmail.com>
2020-12-08 21:47:13 +0100

    jest cos

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        animator.SetFloat("vertical", Input.GetAxis("Vertical"));
+        animator.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Jump();
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.SetBool("run", true);
+        }
+        else
+        {
+            animator.SetBool("run", false);
+        }
+    }
+
+    public void Jump()
+    {
+        animator.SetTrigger("jump");
+    }
+}
